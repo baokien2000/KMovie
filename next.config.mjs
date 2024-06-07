5889174532dfcae187164081e51e9d6dd128cf3a
Bao Kien <kien.tang@poho.live>
2024-06-07 17:19:44 +0700

    perfomance: disable priority

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
         // unoptimized: true,
-        minimumCacheTTL: 60,
+        formats: ['image/avif', 'image/webp'],
         remotePatterns: [
             {
                 protocol: "https",
--- src/components/movies/movies-list.tsx
+++ src/components/movies/movies-list.tsx
@@ -5,10 +5,11 @@ interface MovieListProp {
     movies: ImovieList;
 }
 const MoviesList = ({ movies }: MovieListProp) => {
+    if (!movies.items) return null;
     return (
         <div className={" p-[10px] grid grid-cols-5  gap-x-[10px] rounded-[8px]  w-full bg-[#404040]"}>
             {movies.items.map((movie, index) => {
-                return <MovieCard priority={true} key={movie._id} movie={movie} ImagePath={movies.pathImage} className="w-full h-60 md:h-80" />;
+                return <MovieCard priority={false} key={movie._id} movie={movie} ImagePath={movies.pathImage} className="w-full h-60 md:h-80" />;
             })}
         </div>
     );
