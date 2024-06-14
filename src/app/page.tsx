488af6c7d4425f253b5b35d8a9b8f979aa23f3d7
Bao Kien <kien.tang@poho.live>
2024-06-14 17:36:51 +0700

    perfomance : home test with slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import MovieListTitle from "@/components/movies/movie-list-title";
 import NewMovies from "@/containers/home/new-movies";
-import RecommendedMovies from "@/containers/home/recommended-movies";
 import { getKMovie } from "@/services/movies";
 import { Suspense } from "react";
 import Loading from "./loc-phim/loading";
-// export const dynamic = process.env.NEXT_PUBLIC_MODE === "development" ? "auto" : "force-dynamic";
-export const dynamic = "force-dynamic";
+import nextDynamic from "next/dynamic";
 
+export const dynamic = "force-dynamic";
+import RecommendedMoviesSkeleton from "@/containers/home/recommended-movies-skeleton";
+const MovieSlider = nextDynamic(() => import("@/components/movies/slider"), {
+    ssr: false,
+    loading: () => <RecommendedMoviesSkeleton />,
+});
 export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
     const [initialData, recommended] = await Promise.all([
         getKMovie(searchParams?.page ? parseInt(searchParams.page) : 1, 20, ""),
@@ -14,7 +18,12 @@ export default async function Page({ searchParams }: { searchParams?: { [key: st
     ]);
     return (
         <main className="p-6  ">
-            {/* <RecommendedMovies movies={recommended} /> */}
+            <div className="space-y-1 ">
+                <MovieListTitle title="Phim đề xuất" />
+                <Suspense fallback={<RecommendedMoviesSkeleton />}>
+                    <MovieSlider movies={recommended} />
+                </Suspense>
+            </div>
 
             <div className="space-y-3 ">
                 <MovieListTitle id={"MovieListTitle"} title="Phim mới cập nhật" />
