bc64f8a3501a126e6126984adc2e43bf8802ac13
Bao Kien <kien.tang@poho.live>
2024-06-14 17:16:49 +0700

    perfomance:

--- src/app/tim-kiem/page.tsx
+++ src/app/tim-kiem/page.tsx
@@ -1,13 +1,23 @@
 import MovieListTitle from "@/components/movies/movie-list-title";
 import SearchMovies from "@/containers/search-page/search-movie";
-import React from "react";
+import SearchNotFound from "@/containers/search-page/search-not-found";
+import { getKMovie } from "@/services/movies";
+import React, { Suspense } from "react";
+import Loading from "../loc-phim/loading";
 
 export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
-
+    const searchMovies = await getKMovie(searchParams?.page ? parseInt(searchParams.page) : 1, 20, searchParams?.name);
+    if (!searchMovies || !searchParams?.name) return <SearchNotFound />;
     return (
         <main className="p-6 space-y-4  ">
             <MovieListTitle id="SearchListTitle" title={<>Tìm kiếm theo từ khóa {<b className="text-mainColor">{searchParams?.name}</b>}</>} />
-            <SearchMovies  searchParams={searchParams} />
+            <Suspense fallback={<Loading />}>
+                {!searchMovies && searchMovies?.items?.length === 0 ? (
+                    <SearchNotFound />
+                ) : (
+                    <SearchMovies initialData={searchMovies} searchParams={searchParams} />
+                )}
+            </Suspense>
         </main>
     );
 }
--- src/containers/search-page/search-movie.tsx
+++ src/containers/search-page/search-movie.tsx
@@ -1,40 +1,32 @@
 "use client";
-import React from "react";
-import { getKMovie } from "@/services/movies";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { useEffect } from "react";
 import MoviePagination from "@/components/movies/movie-pagination";
+import { ImovieList } from "@/interface/movies";
 import NewMovieSkeleton from "../home/new-movie-skeleton";
 import MoviesList from "@/components/movies/movies-list";
 import { createQueryString } from "@/utils/format-string";
 import { usePathname, useRouter } from "next/navigation";
 import { scrollToTitleId } from "@/utils/scroll";
 
-const SearchMovies = ({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) => {
-    const queryClient = useQueryClient();
+const SearchMovies = ({ searchParams, initialData }: { searchParams?: { [key: string]: string | undefined }; initialData: ImovieList }) => {
+    const [loading, setLoading] = React.useState(false);
     const router = useRouter();
     const pathname = usePathname();
-
-    const { data: movies, isFetching } = useQuery({
-        queryKey: ["getMoviesSearch", searchParams?.page, searchParams?.name],
-        queryFn: async () => getKMovie(searchParams?.page ? parseInt(searchParams.page) : 1, 20, searchParams?.name),
-        refetchOnWindowFocus: false,
-    });
-
+    useEffect(() => {
+        setLoading(false);
+    }, [initialData?.pagination?.currentPage]);
     const handlePageClick = (data: { selected: number }) => {
         const queryString = createQueryString(searchParams, "page", (data.selected + 1).toString());
-        queryClient.invalidateQueries({
-            queryKey: ["getMoviesSearch", data.selected + 1, searchParams?.name],
-        });
-        router.replace(`${pathname}?${queryString}`, { scroll: false });
         scrollToTitleId("SearchListTitle");
+        router.replace(`${pathname}?${queryString}`, { scroll: false });
     };
-    return !isFetching ? (
-        <>
-            <MoviesList quality={50} enableBlur movies={movies} />
-            <MoviePagination onPageClick={handlePageClick} totalPage={Math.ceil(movies?.pagination?.totalPages ?? 0)} />
-        </>
-    ) : (
+    return loading ? (
         <NewMovieSkeleton />
+    ) : (
+        <>
+            <MoviesList quality={50} enableBlur movies={initialData} />
+            <MoviePagination onPageClick={handlePageClick} totalPage={Math.ceil(initialData?.pagination?.totalPages ?? 0)} />
+        </>
     );
 };
 
