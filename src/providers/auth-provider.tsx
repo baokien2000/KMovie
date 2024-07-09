9c05dfa3930cf3e3ae37b915287525e684ad18d1
baokien2000 <baokien2000@gmail.com>
2024-07-09 17:27:49 +0700

    test: auth ref token

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -10,10 +10,16 @@ const AuthProvider = ({ refreshToken }: { refreshToken: string | undefined }) =>
     const user = useAuthStore((state) => state.user);
     const setToken = useAuthStore((state) => state.updateUserToken);
     const setUser = useAuthStore((state) => state.setUser);
-    console.log("refreshToken outside", refreshToken);
+    let token = refreshToken;
+    console.log("refreshToken outside", token);
     const tokenCheck = async () => {
-        console.log("refreshToken inside", refreshToken);
-        if ((refreshToken && isTokenExpired(refreshToken)) || !refreshToken) {
+        console.log("refreshToken inside", token);
+        if (!refreshToken) {
+            const res = await axios({ url: "/api", method: "get", withCredentials: true });
+            token = res?.data?.token?.value || token;
+            console.log("new token", token);
+        }
+        if ((token && isTokenExpired(token)) || !token) {
             setUser(null);
             await axios({ url: "/api", method: "delete", withCredentials: true });
             toast.error("Phiên đăng nhập hết hạn, vui lòng đăng nhập lại");
