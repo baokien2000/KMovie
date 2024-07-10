8541112256de8b537691d90f3662d82d523fde46
baokien2000 <baokien2000@gmail.com>
2024-07-10 16:24:48 +0700

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,8 @@ import MoviesSliderSkeleton from "@/components/movies/slider/movies-slider-skele
 import SliderContainer from "@/components/movies/slider/slider-container";
 import HomeContainer from "@/containers/home/home-container";
 
-// export const dynamic = process.env.NEXT_PUBLIC_MODE === "development" ? "auto" : "force-dynamic";
-// const MovieSlider = nextDynamic(() => import("@/components/movies/slider/slider"), {
-//     ssr: false,
-//     loading: () => <MoviesSliderSkeleton />,
-// });
 
 export default async function Page({ searchParams }: { searchParams?: { [key: string]: string | undefined } }) {
-    // const initialData = await getKMovie(searchParams?.page ? parseInt(searchParams.page) : 1, pageSize, "");
     return (
         <main className="md:p-6 sm:p-3 py-3  ">
             <div className="space-y-3 ">
